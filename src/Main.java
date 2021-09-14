dcb8e6637d28c4fdf5e29ab51c36a60b8911512d
dev-viniciuss <dev.mvsc@gmail.com>
2021-09-14 15:45:44 -0300

    Melhorando as saídas e instacionando a classe de usuário

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Matriz sudoku = new Matriz();
+        User usuario = new User();
 
+        usuario.perguntarNome();
+
+        System.out.println("Está é o tabuleiro gerado para você jogar!");
         sudoku.imprimirMatriz();
+
+        System.out.println(" ");
+
+        while (true) {
+            sudoku.jogada();
+            if (sudoku.getProsseguir() == false) {
+                break;
+            };
+        }
 
     }
 }
