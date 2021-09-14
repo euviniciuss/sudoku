64143226f88fe5847e9838eba47066845a20756f
dev-viniciuss <dev.mvsc@gmail.com>
2021-09-14 15:44:56 -0300

    Criando classe de usuário

--- src/User.java
+++ src/User.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class User {
+import java.util.Scanner;
+
+public class User {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void perguntarNome() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Qual o nome do nosso jogador(a)? ");
+        String user_nome = scan.nextLine();
+
+        this.nome = user_nome;
+    };
+
+    public void imprimirNome() {
+        System.out.println(this.nome);
+    };
 }
